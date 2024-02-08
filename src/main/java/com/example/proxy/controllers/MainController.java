ad320cf0b52640e1d0b22bb5607d69014ec58e16
StOlegsey <2001mishka@gmail.com>
2024-02-08 21:59:09 +0300

    just displaying the page

--- src/main/java/com/example/proxy/controllers/MainController.java
+++ src/main/java/com/example/proxy/controllers/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Enumeration;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
@@ -16,20 +18,37 @@ public class MainController {
     private String InitUrl = null;
 
     @RequestMapping(value = "/**")
-    public ResponseEntity<String> proxyRequest(@RequestBody(required = false) String body,
+    public ResponseEntity<byte[]> proxyRequest(@RequestBody(required = false) String body,
                                                @RequestParam(required = false) String url,
+                                               @RequestParam Map<String, String> additionalParams,
                                                HttpMethod method,
                                                HttpServletRequest request) {
         if(url != null) {InitUrl = url;}
 
-        System.out.println("method: "+method+"; request: localhost"+ (request.getRequestURI()));
+        System.out.println("method: "+method+"; request: "+ (request.getRequestURI()));
 
-        Connection connection = new Connection(InitUrl+request.getRequestURI(), new RestTemplate());
-        ResponseEntity<String> response = connection.makeRequest(body,method,request);
+        /*if(Objects.equals(request.getRequestURI(), "/search")) {
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                Enumeration<String> values = request.getHeaders(headerName);
+
+                while (values.hasMoreElements()) {
+                    String value = values.nextElement();
+                    System.out.println("HeaderName: " + headerName + " value: " + value);
+                }
+            }
+        }*/
+        //for (Map.Entry<String, String> entry: additionalParams.entrySet()) {
+        //    System.out.println(entry.getKey() + ":" + entry.getValue());
+        //}
+
+        Connection connection = new Connection(InitUrl+request.getRequestURI());
+        ResponseEntity<byte[]> response = connection.makeRequest(body,method, additionalParams, request);
 
         try {
-            System.out.println("Response body Length: "+response.getBody().length()+
-                    "\nResponse headers: "+response.getHeaders());
+            System.out.println("Response body Length: "+response.getBody().toString().length()+
+                    "\nResponse ContentType: "+response.getHeaders().getContentType());
         }
         catch (NullPointerException exception){
             System.out.println("Response is null");
--- src/main/java/com/example/proxy/network/Connection.java
+++ src/main/java/com/example/proxy/network/Connection.java
@@ -1,33 +1,63 @@
 package com.example.proxy.network;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Enumeration;
+import java.util.Map;
+import java.util.Objects;
 
 public class Connection {
     private final String  url;
-    private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    public Connection(String url, RestTemplate restTemplate) {
+    public Connection(String url) {
         this.url = url;
-        this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<String> makeRequest(String body,
+    public ResponseEntity<byte[]> makeRequest(String body,
                                               HttpMethod method,
+                                              Map<String, String> additionalParams,
                                               HttpServletRequest request){
-        HttpHeaders headers = new HttpHeaders();//getHeaders(request);
+        HttpHeaders headers = getHeaders(request);
         //System.out.println("HEADERS default: "+headers);
         HttpEntity<String> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, method, entity, String.class);
-        System.out.println("Made a request to: "+url);
 
-        return ResponseEntity.status(response.getStatusCode()).headers(response.getHeaders()).body(response.getBody());
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
+        for (Map.Entry<String, String> entry: additionalParams.entrySet()) {
+                uriBuilder.queryParam(entry.getKey(), entry.getValue());
+            }
+
+        System.out.println("Making a request to: "+uriBuilder.toUriString());
+
+        try {
+            ResponseEntity<byte[]> response = restTemplate.exchange(
+                    uriBuilder.toUriString(),
+                    method,
+                    entity,
+                    byte[].class);
+
+            return ResponseEntity.status(response.getStatusCode())
+                    .contentType(Objects.requireNonNull(response.getHeaders().getContentType()))
+                    .headers(response.getHeaders())
+                    .body(response.getBody());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            return ResponseEntity.status(e.getStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsByteArray());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .body(("Unexpected error: " + e.getMessage()).getBytes());
+        }
+
     }
 
     private HttpHeaders getHeaders(HttpServletRequest request){
